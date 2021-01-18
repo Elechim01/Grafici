b2f772105126885d07bef5175374b3bac1cb69d7
Elechim01 <mikymanny015@gmail.com>
2021-01-18 22:04:39 +0100

    Initial Commit

---
+++ Grafici/GraficiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraficiApp.swift
+//  Grafici
+//
+//  Created by Michele on 18/01/21.
+//
+
+import SwiftUI
+
+@main
+struct GraficiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
